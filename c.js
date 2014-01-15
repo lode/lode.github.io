23795ea9c577b5c6abba204ef8dd919f10477be1
Lode Claassen <lode@alsvanzelf.nl>
2014-01-15 18:32:38 +0100

    uses the original photo for the bigger sized one

--- c.js
+++ c.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+$('header img').on('click', function(){
+	var update  = ($(this).attr('class') == 'thumb') ? 'full' : 'thumb';
+	
+	$(this).attr('src', $(this).attr('data-' + update));
+	$(this).removeClass().addClass(update);
+});
+
 $('#contactdetails a').on('click', function(){
 	$('#contactdetails a').toggle();
 	
